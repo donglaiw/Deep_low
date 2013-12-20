5ae217c2e442c33d0758e07bb96da3fe3745e520
Eminemya <weiddoonngglai@gmail.com>
2013-12-20 00:07:27 -0500

    do it

--- dn/P_MLP.py
+++ dn/P_MLP.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     ishape = Conv2DSpace(shape = (17,17),num_channels = 1)        
     param = SetParam()    
     
-    p_fc = param.param_model_fc(dim = 1000,irange=0.1,layer_type=0)    
+    p_sig = param.param_model_fc(dim = 289,irange=0.1,layer_type=0)    
     #p_cf = param.param_model_cf(n_classes = nclass,irange=0.1)        
-    p_linear = param.param_model_fc(dim = nclass,irange=0.1,layer_type=2)        
-    
+    #p_linear = param.param_model_fc(dim = nclass,irange=0.1,layer_type=2)        
+    p_fc = [p_sig]
+
     p_algo = param.param_algo(batch_size = 1000,
-                             termination_criterion=EpochCounter(max_epochs=10),
+                             termination_criterion=EpochCounter(max_epochs=2),
                             #cost=Dropout(input_include_probs={'l1': .8},input_scales={'l1': 1.}),                             
                              learning_rate=0.001,
                              batches_per_iter =9,
@@ -30,9 +31,7 @@ if __name__ == "__main__":
     
     np.random.seed(1)
     rand_ind = np.random.permutation([i for i in range(100000)])
-    print "lll"
     net.loaddata(DD,2,nclass,rand_ind[:90000],rand_ind[90000:])
-    print "lll2"
-    net.setup([[p_fc,p_linear]],p_algo)
+    net.setup([p_fc],p_algo)
     
     net.train()
