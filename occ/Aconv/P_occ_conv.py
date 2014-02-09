34ef3e4d8057db8c37d60b7648eabc96de22c459
Eminemya <weiddoonngglai@gmail.com>
2014-02-08 23:00:16 -0500

    full model

--- occ/Aconv/P_occ_conv.py
+++ occ/Aconv/P_occ_conv.py
@@ -25,14 +25,15 @@ class Deep_occ(DBL_model):
                 self.psz = 11
             elif self.model_id<=4:
                 self.psz = 15
-            elif self.model_id<=4:
-                self.psz = 15
             elif self.model_id<=5:
                 self.psz = 17
+            elif self.model_id<=6:
+                self.psz = 15
             self.ishape = Conv2DSpace(shape = (self.psz,self.psz),num_channels = 3)
 
-            if (self.model_id==3 and self.num_dim[-1]==4) or (self.model_id==2 and self.num_dim[-1]==1):
+            if (self.model_id==6 and self.num_dim[-1]==4) or (self.model_id==3 and self.num_dim[-1]==4) or (self.model_id==2 and self.num_dim[-1]==1):
                 self.p_data['pre_id'] = 1
+                self.p_data['im_id'] = 1
         elif self.train_id <= 4:
             # contour completion
             if self.train_id==3:
@@ -260,6 +261,20 @@ class Deep_occ(DBL_model):
                 self.param.param_model_conv(self.num_dim[2],ks[2],ps[2],pd[2],ir[2],layer_type=0)],
                 [self.param.param_model_fc(dim = self.num_dim[3],irange=n1,layer_type=2)]
                 ]
+        elif self.model_id ==6:        
+            ks = [[11,11],[5,5],[1,1],[1,1],[1,1]]
+            ir = [0.05,0.05,0.05,0.05,0.05]
+            ps = [[1,1],[1,1],[1,1],[1,1],[1,1]]
+            pd = [[1,1],[1,1],[1,1],[1,1],[1,1]]
+            n1 = 0.01
+            self.p_layers = [
+                [self.param.param_model_conv(self.num_dim[0],ks[0],ps[0],pd[0],ir[0],layer_type=0),
+                self.param.param_model_conv(self.num_dim[1],ks[1],ps[1],pd[1],ir[1],layer_type=0),
+                self.param.param_model_conv(self.num_dim[2],ks[2],ps[2],pd[2],ir[2],layer_type=self.num_dim[5]),
+                self.param.param_model_conv(self.num_dim[3],ks[3],ps[3],pd[3],ir[3],layer_type=self.num_dim[5]),
+                self.param.param_model_conv(self.num_dim[4],ks[4],ps[4],pd[4],ir[4],layer_type=self.num_dim[5])]
+                ]
+
 
     def buildAlgo(self):
         if self.algo_id == 0:
