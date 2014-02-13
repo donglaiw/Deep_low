b525dd036b7053e3da41268ae4babc9dfed0fa7f
Eminemya <weiddoonngglai@gmail.com>
2014-02-13 17:55:11 -0500

    02_13

--- occ/Aconv/P_occ_conv.py
+++ occ/Aconv/P_occ_conv.py
@@ -229,10 +229,10 @@ class Deep_occ(DBL_model):
             pre =self.result_mat[:-5]
             #self.p_data['data_id'] = 11
             #self.p_data['data'] = ['bd_st2.mat']
-            self.p_data['data_id'] = 12
-            self.p_data['data'] = ['dn_ucb_Is2.mat']
-            #self.p_data['data_id'] = 9
-            #self.p_data['data'] = ['dn_ucb2.mat']
+            #self.p_data['data_id'] = 12
+            #self.p_data['data'] = ['dn_ucb_Is2.mat']
+            self.p_data['data_id'] = 9
+            self.p_data['data'] = ['dn_ucb2.mat']
             #self.p_data['data'] = ['dn_ucb0.mat']
             tid = self.test_id-1
             for i in range(50*tid,50*(tid+1)):
